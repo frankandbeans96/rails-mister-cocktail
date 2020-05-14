ed6ea321cf8a1b4c99c6109d0384d11c2961bbff
Francois <francois.desazars@gmail.com>
2020-05-14 17:30:45 +0200

    DRY on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/create'
-  get 'cocktails/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:show, :index, :create, :new]
 end
